47c3a175bb6e35d4489a500bbafa58259125ba23
gael-src <src.gael@gmail.com>
2024-01-27 23:40:25 +0100

    update

--- main.js
+++ main.js
@@ -4,9 +4,16 @@ const redirectIfShortsUrl = (details) => {
 
   // Check if the extension is active
   chrome.storage.sync.get({ isActive: true }, ({ isActive }) => {
+    console.log("Extension is active:", isActive);
+
     if (isActive && url.pathname.includes("/shorts/")) {
+      console.log("Redirecting from:", url.href);
+
       // Redirect to "/watch/" by replacing "/shorts/" with "/watch/"
       const newUrl = url.href.replace("/shorts/", "/watch/");
+
+      console.log("Redirecting to:", newUrl);
+
       // Cancel the navigation and redirect
       chrome.webNavigation.onBeforeNavigate.removeListener(redirectIfShortsUrl);
       chrome.tabs.update(details.tabId, { url: newUrl });
